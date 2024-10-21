a43e68ce1298b9cdd926b9937bfd88ad989364be
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-10-21 15:39:20 +0000

    create shopping bag and context

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -13,7 +15,6 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
-    
 
     if request.GET:
         if 'sort' in request.GET:
