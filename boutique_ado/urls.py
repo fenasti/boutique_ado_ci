f82d3111cdfa27f537dfa172b4404f8226604df5
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-10-31 14:40:46 +0000

    add profile accont mnagment

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('bag/', include('bag.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
