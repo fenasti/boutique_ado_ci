72e58391ea37c0d6fc57a87d447c141bb592139e
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-01 17:47:06 +0000

    debug commit

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52v102ri-)+_669v29qy8@_%q%56m_qw#5y*&7xw+(bv=7cg)o'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-fenasti-boutiqueadoci-87ora13tbj2.ws.codeinstitute-ide.net',
